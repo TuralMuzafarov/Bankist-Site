2a56722630b91ddd5eb8be9615f333cc110ab67a
TuralMuzafarov <turalmuzefferli4@gmail.com>
2024-08-19 17:28:50 +0400

    LAZY LOADING

--- script.js
+++ script.js
@@ -116,7 +116,6 @@ const allSections = document.querySelectorAll('.section');
 
 const revealSection = function (entries, observer){
   const [entry] = entries;
-  console.log(entry);
 
   if(!entry.isIntersecting) return;
   entry.target.classList.remove('section--hidden');
@@ -132,3 +131,26 @@ allSections.forEach(function(section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden')
 });
+
+// LAZY LOADING IMAGES
+
+const imgTargets = document.querySelectorAll('img[data-src]');
+console.log(imgTargets)
+
+const loadImg = function (entries, observer){
+  const [entry] = entries;
+
+  if(!entry.isIntersecting) return;
+
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function (){
+    entry.target.classList.remove('lazy-img');
+  })
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0
+});
+imgTargets.forEach(img => imgObserver.observe(img));
